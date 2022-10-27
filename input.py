99ac4b454abdb7e6e928c12025ae10fc28472190
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-10-26 20:53:27 -0600

    Create input.py

---
+++ input.py
@@ -0,0 +1,10 @@
+print("Blue or green?")
+
+choice = input()
+
+if choice == 'blue':
+    print("The sky is blue")
+elif choice == 'green':
+    print("The grass is green")
+else:
+    print("Thats not an option dummy")
