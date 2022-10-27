4f34e8532e82a551dc96596697d2582019289463
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-10-26 21:12:44 -0600

    Create number input.py

---
+++ number input.py
@@ -0,0 +1,9 @@
+print("How old is u?")
+a=int(input())
+
+if a<43:
+        print("You're to young go watch some tak tik")
+elif a==43:
+        print("Ah yes the perfect number")
+else:
+    print("Youre to old go eat some oatmeal")
